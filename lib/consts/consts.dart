ef42f6306de768de8d52bce3511c345841a176c9
swagatapal <swagatapal1980@gmail.com>
2024-01-17 22:47:25 +0530

    initial

---
+++ lib/consts/consts.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: depend_on_referenced_packages
+
+export './colors.dart';
+export './images.dart';
+export './strings.dart';
+export './styles.dart';
+export 'package:velocity_x/velocity_x.dart';
