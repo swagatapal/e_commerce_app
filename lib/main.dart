45a507144a3d7f688aec561750196afdb7624c7c
swagatapal <swagatapal1980@gmail.com>
2024-01-22 18:30:45 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:e_commerce_app/consts/consts.dart';
 import 'package:e_commerce_app/views/splash_screen/splashScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
